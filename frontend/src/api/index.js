78df63e955675da1e69beebc5fd385a47a5c598c
Martín Comito <martin@cohley.com>
2026-05-21 14:53:23 -0300

    Configuraciones base de frontend

---
+++ frontend/src/api/index.js
@@ -0,0 +1,5 @@
+const BASE_URL = '/api';
+
+// Las funciones para llamar a la API se agregarán aquí
+
+export default BASE_URL;
---
+++ frontend/src/pages/Ventas/index.jsx
@@ -0,0 +1,5 @@
+function VentasPage() {
+  return <div>Módulo de Ventas</div>;
+}
+
+export default VentasPage;
