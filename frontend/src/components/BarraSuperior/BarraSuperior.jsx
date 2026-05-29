e55f6c0b9962b27eca7571e8a43ca1768038323c
Martín Comito <martin@cohley.com>
2026-05-29 11:44:09 -0300

    Moví imágenes de /public a /img, cambié imagen del logo por una transparente

--- frontend/src/components/BarraSuperior/BarraSuperior.jsx
+++ frontend/src/components/BarraSuperior/BarraSuperior.jsx
@@ -2,34 +2,28 @@ import "./BarraSuperior.scss";
 import { Link } from "react-router-dom";
 
 const BarraSuperior = () => {
-    return (
-        <header className="barra-superior">
-            <div className="barra-superior__logo">
-                <Link to="/">
-                    <img
-                        src="/videovinyl-logo.png"
-                        alt="VideoVinyl Logo"
-                    />
-                </Link>
-            </div>
+  return (
+    <header className="barra-superior">
+      <div className="barra-superior__logo">
+        <Link to="/">
+          <img src={"/src/img/videovinyl-logo.png"} alt="VideoVinyl Logo" />
+        </Link>
+      </div>
 
-            <div className="barra-superior__usuario">
-                <div className="barra-superior__datos">
-                    <span className="nombre">Admin Usuario</span>
-                    <span className="email">admin@videovinyl.com</span>
-                </div>
+      <div className="barra-superior__usuario">
+        <div className="barra-superior__datos">
+          <span className="nombre">Admin Usuario</span>
+          <span className="email">admin@videovinyl.com</span>
+        </div>
 
-                <div className="barra-superior__avatar">
-                    <Link to="/">
-                        <img
-                            src="/user-icon.jpg"
-                            alt="USUARIO"
-                        />
-                    </Link>
-                </div>
-            </div>
-        </header>
-    );
+        <div className="barra-superior__avatar">
+          <Link to="/">
+            <img src={"/src/img/user-icon.jpg"} alt="USUARIO" />
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default BarraSuperior;
