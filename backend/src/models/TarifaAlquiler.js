817e61c6ff97025a29942a9623d805524ac8cc7d
Martín Comito <martin@cohley.com>
2026-06-08 19:34:28 -0300

    Modifiqué el ínidice de la tabla TarifaAlquiler

--- backend/src/models/TarifaAlquiler.js
+++ backend/src/models/TarifaAlquiler.js
@@ -5,7 +5,6 @@ const TarifaAlquiler = sequelize.define('TarifaAlquiler', {
   tipo: {
     type: DataTypes.ENUM('DVD', 'VHS'),
     allowNull: false,
-    unique: true,
   },
   precio_por_dia: {
     type: DataTypes.DECIMAL(10, 2),
@@ -14,6 +13,7 @@ const TarifaAlquiler = sequelize.define('TarifaAlquiler', {
 }, {
   tableName: 'tarifas_alquiler',
   timestamps: true,
+  indexes: [{ unique: true, fields: ['tipo'] }],
 });
 
 export default TarifaAlquiler;
