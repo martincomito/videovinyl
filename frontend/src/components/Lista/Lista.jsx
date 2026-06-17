1fae24f0be40d0368e6eb5a0fe95c491f65538cb
Lucrecia Pedraza <lucre.ph@hotmail.com>
2026-06-17 19:53:56 -0300

    creacion de pagina de reportes e historial

--- frontend/src/components/Lista/Lista.jsx
+++ frontend/src/components/Lista/Lista.jsx
@@ -6,6 +6,7 @@ function Lista({
     columnas = [],
     datos = [],
     mostrarBuscador = false,
+    titulo,
 }) {
     const registrosPorPagina = 10;
 
@@ -58,6 +59,14 @@ function Lista({
                 bg-[var(--color-lista-fondo)]
             "
         >
+            {titulo && (
+              <div className="px-4 pt-4 pb-2">
+                <h2 className="text-sm font-semibold text-[var(--color-texto-primario)]">
+                  {titulo}
+                </h2>
+              </div>
+            )}
+
             {mostrarBuscador && (
                 <div
                     className="
